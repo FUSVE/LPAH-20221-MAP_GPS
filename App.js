70657d8cd29efb976e4711eb04cacf89dc76e435
Tassio Sirqueira <tassio@tassio.eti.br>
2022-03-18 08:01:36 -0300

    .
    .

---
+++ App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+
+export default function App() {
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permissão da localização negada!');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+    })();
+  }, []);
+
+  let text = 'Aguarde...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView loadingEnabled={true}
+        region={
+          !location ?
+            {
+              latitude: 0,
+              longitude: 0,
+              latitudeDelta: 0,
+              longitudeDelta: 1000,
+            } :
+            {
+              latitude: location.latitude,
+              longitude: location.longitude,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
+            }
+        }
+        style={styles.map}
+      >
+        <Marker coordinate={
+           !location ?
+           {
+             latitude: 0,
+             longitude: 0,
+             latitudeDelta: 0,
+             longitudeDelta: 1000,
+           } :
+           {
+             latitude: location.latitude,
+             longitude: location.longitude,
+             latitudeDelta: 0.005,
+             longitudeDelta: 0.005,
+           }
+        }
+        title="Eu estou aqui!"
+        description='Nosso local de aula.'
+        />
+      </MapView>
+      <Text style={styles.paragraph}>{text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: "100%",
+    height: "80%",
+  },
+});
